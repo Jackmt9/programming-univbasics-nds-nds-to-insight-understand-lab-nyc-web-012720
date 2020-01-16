c6889b2bdb6d67143332d158d023c731897e7570
Jack T <jackmt9@gmail.com>
2020-01-16 23:02:55 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,7 +11,7 @@ end
 
 def print_first_directors_movie_titles
   directors_database[0][:movies].each do |movie|
-    puts movie[0][:title]
+    puts puts directors_database[0][:movies][movie][:title]
   end
   puts directors_database[0][:movies][0][:title]
   puts directors_database[0][:movies][1][:title]
